03f1556978439d5db6e26ca62e4727fc7cf2b6be
Aman Tiwari <88995970+mnn2003@users.noreply.github.com>
2025-02-17 18:00:55 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     <Router>
       <div className="min-h-screen bg-gray-50 flex flex-col">
         <Navbar />
-        <main className="flex-grow">
+        <main className="flex-grow pt-16">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/events" element={<Events />} />
