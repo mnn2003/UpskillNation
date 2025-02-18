f89c995709e7787ff4c9df16a05a6d178b24c031
Aman Tiwari <88995970+mnn2003@users.noreply.github.com>
2025-02-18 17:13:51 +0530

    Update LearnForm.tsx

--- src/components/admin/LearnForm.tsx
+++ src/components/admin/LearnForm.tsx
@@ -102,7 +102,7 @@ const LearnForm = ({ newContent, setNewContent, handleCreateContent, onCancel }:
           </button>
           <button
             type="submit"
-            className="px-4 py-2 border border-transparent rounded-lg shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700"
+            className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700"
           >
             Create Resource
           </button>
