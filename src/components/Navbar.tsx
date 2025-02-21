26290b55fc7e5bf2df585ba1bdbf093df6a5709b
Aman Tiwari <88995970+mnn2003@users.noreply.github.com>
2025-02-21 19:03:36 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,11 +28,12 @@ const Navbar = () => {
   }, [lastScrollY]);
 
   return (
-    <nav
-      className={bg-white shadow-sm transition-all duration-300 ${
-        isVisible ? "top-0" : "-top-16" 
-      } fixed w-full z-50}
-    >
+     <nav
+    className={`bg-white shadow-sm transition-all duration-300 ${
+      isVisible ? "top-0" : "-top-16"
+    } fixed w-full z-50`}
+  >
+
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           {/* Logo */}
